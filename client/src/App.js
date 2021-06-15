93231aab404a0a2960c2dc4679c996d769e9fdb5
john-running <john.a.running@gmail.com>
2021-06-15 09:55:19 -0400

    intial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <div className="ui pointing menu">
+                    <Link className="item" to="/">
+                        Home
+                    </Link>
+                    <Link className="item" to="/otherpage">
+                        Other Page
+                    </Link>
+                </div>
+                <div>
+                    <Route exact path="/" component={Fib} />
+                    <Route path="/otherpage" component={OtherPage} />
+                </div>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
